377d7f1179d9ed2c286bf5fc236706579a79fd4b
cjj <chenjianjun571@gmail.com>
2016-02-22 18:35:18 +0800

    婚纱礼服详情页面修改

--- components/dress-details.jsx
+++ components/dress-details.jsx
@@ -2,26 +2,38 @@ import React, { PropTypes } from 'react'
 import _ from 'lodash'
 
 const DressDetails = React.createClass({
-  render: function() {
-    return (
-      <div class="hslf-xq-view">
-          <div class="layout-center-box">
-              <ul class="list-recommend">
-
-                <li class="item-box">
-                    <a class="img-box" href="">
-                      <img src="http://image.jsbn.com/WebImage/cq/jpg/20150928/77520046727262073264/20150928162340001190_945x1418.jpg@550h_90Q" />
-                    </a>
-                    <div class="title-box">
-                      <span>护肤</span>
-                    </div>
-                </li>
-
-              </ul>
+    render() {
+        return (
+          <div className="hslf-xq-view">
+              <div className="layout-center-box">
+                  <ul className="list-recommend">
+                      {
+                          _.map(this.props.dressItems, (v,k) => {
+                              return (
+                                  <li className="item-box">
+                                      <a className="img-box" href="">
+                                          <img src="http://image.jsbn.com/WebImage/cq/jpg/20150928/77520046727262073264/20150928162340001190_945x1418.jpg@550h_90Q" />
+                                      </a>
+                                      <div className="title-box">
+                                          <span>护肤</span>
+                                      </div>
+                                  </li>
+                              );
+                          })
+                      }
+                  </ul>
+              </div>
           </div>
-      </div>
-    );
-  }
+        );
+    },
+    propTypes: {
+        dressItems: PropTypes.array
+    },
+    getDefaultProps(){
+        return {
+            dressItems:[{"12":12},{"13":13}]
+        }
+    }
 });
 
 export { DressDetails };
