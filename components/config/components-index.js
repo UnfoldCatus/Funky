0bf2d855cafa576e2152ba4bec69a49a58e4ab5c
eavlee <422628422@qq.com>
2016-02-24 10:54:47 +0800

    movie.jsx repost

--- components/config/components-index.js
+++ components/config/components-index.js
@@ -6,80 +6,26 @@ value为components模块
 
 TODO: 使用bash扫描component文件夹直接生成本文件
 */
-import React, {
-  PropTypes
-}
-from 'react'
+import React, { PropTypes } from 'react'
 
-import {
-  Home
-}
-from '../home.jsx'
-import {
-  Shot
-}
-from '../shot.jsx'
-import {
-  Sample
-}
-from '../sample.jsx'
-import {
-  Pringles
-}
-from '../pringles.jsx'
-import {
-  Suite
-}
-from '../suite.jsx'
-import {
-  Hotel
-}
-from '../hotel.jsx'
-import {
-  HotelRequire
-}
-from '../hotel-require.jsx'
-import {
-  Scheme
-}
-from '../scheme.jsx'
-import {
-  Cases
-}
-from '../cases.jsx'
-import {
-  WeddingPat
-}
-from '../wedding-pat.jsx'
-import {
-  WeddingVideo
-}
-from '../wedding-video.jsx'
-import {
-  SchemeRequire
-}
-from '../scheme-require.jsx'
+import { Home } from '../home.jsx'
+import { Shot } from '../shot.jsx'
+import { Sample } from '../sample.jsx'
+import { Pringles } from '../pringles.jsx'
+import { Suite } from '../suite.jsx'
+import { Hotel } from '../hotel.jsx'
+import { HotelRequire } from '../hotel-require.jsx'
+import { Scheme } from '../scheme.jsx'
+import { Cases } from '../cases.jsx'
+import { WeddingPat } from '../wedding-pat.jsx'
+import { WeddingVideo } from '../wedding-video.jsx'
+import { SchemeRequire } from '../scheme-require.jsx'
 
-import {
-  Adv
-}
-from '../adv.jsx'
-import {
-  Dress
-}
-from '../dress.jsx'
-import {
-  DressDetails
-}
-from '../dress-details.jsx'
-import {
-  Movie
-}
-from '../movie.jsx'
-import {
-  Supply
-}
-from '../supply.jsx'
+import { Adv } from '../adv.jsx'
+import { Dress } from '../dress.jsx'
+import { DressDetails } from '../dress-details.jsx'
+import { Movie } from '../movie.jsx'
+import { Supply } from '../supply.jsx'
 
 
 const ComponentsIndex = {
