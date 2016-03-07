ee8e7c1a3d548b5bf6c9b3b122d6d9eed7523aeb
cjj <chenjianjun571@gmail.com>
2016-03-07 16:05:55 +0800

    提交微电影

--- components/movie.jsx
+++ components/movie.jsx
@@ -5,58 +5,6 @@ import { MediaItem } from './common/media-item.jsx'
 import { Banner } from './common/banner.jsx'
 import { MovieConfig } from './config/movie-config'
 
-//const Item = React.createClass({
-//  render: function() {
-//    return (
-//      <li className="item-box">
-//        <div className="animat-1-hive" />
-//        <a href={this.props.detail}>
-//          <div className="img-box">
-//            <img src={this.props.cover} />
-//            <i />
-//          </div>
-//          <div className="item-info" style={{width:'320px'}}>
-//            <h2>{this.props.title}</h2>
-//            <p>{this.props.remark}</p>
-//            <span className="more">
-//            </span>
-//          </div>
-//        </a>
-//      </li>
-//    )
-//  }
-//});
-//
-//const ItemList = React.createClass({
-//  render: function() {
-//    var self = this
-//    return (
-//      <ul className='list-recommend'>
-//        {
-//            _.map(this.props.list,function(v,k){
-//                return <Item title={v.name} detail={'#'+self.getPathname()+'/'+v.videoId} cover={v.coverImage.imageUrl} key={k} remark={v.remark}/>
-//            })
-//        }
-//      </ul>
-//    );
-//  },
-//  getDefaultProps: function() {
-//    return {
-//      list: []
-//    }
-//  }
-//});
-
-
-//_.map(this.state.brands, (v, k) => {
-//  var boundClick = this.handleClick.bind(this, k);
-//  return (
-//    <li key={k} onClick={boundClick} className={(k === this.state.index)? 'item item-sel':'item'} >
-//      <img src={v.logoUrl} />
-//    </li>
-//  );
-//})
-
 const ItemType = React.createClass({
   render () {
     let listType = this.props.listType;
@@ -66,7 +14,7 @@ const ItemType = React.createClass({
           <img src="http://image.jsbn.com/static/wdy-nav.png" />
           <ul className="hover-box">
             {
-              _.map(listType, (v,k) => {
+              _.map(this.props.type, (v,k) => {
                 var boundClick = this.handleClick.bind(this, k, v);
                 return (
                   <li key={k} onClick={boundClick} className={(k === this.state.index)? 'hover-item sel':'hover-item'}>
@@ -78,27 +26,27 @@ const ItemType = React.createClass({
           </ul>
         </div>
         <ul className="list-recommend">
-            {
-              _.map(this.state.data[this.state.index], (v,k) => {
-                let key = ''+this.state.index+k;
-                return (
-                  <li key={key} className="item-box">
-                    <div className="animat-1-hive" />
-                      <a href="">
-                        <div className="img-box">
-                          <MediaItem aspectRatio='38:25' height={250} mediaUrl={v.coverUrlWeb} />
-                          <i></i>
-                        </div>
-                        <div className="item-info">
-                          <h2>{v.name}</h2>
-                          <p>{v.description}</p>
-                          <span className="more">&gt 更多详情</span>
-                        </div>
-                      </a>
-                  </li>
-                );
-              })
-            }
+          {
+            _.map(this.state.data[this.state.index], (v,k) => {
+              let key = ''+this.state.index+k;
+              return (
+                <li key={key} className="item-box">
+                  <div className="animat-1-hive" />
+                  <a href="">
+                    <div className="img-box">
+                      <MediaItem aspectRatio='38:25' height={250} mediaUrl={v.coverUrlWeb} />
+                      <i></i>
+                    </div>
+                    <div className="item-info">
+                      <h2>{v.name}</h2>
+                      <p>{v.description}</p>
+                      <span className="more">&gt 更多详情</span>
+                    </div>
+                  </a>
+                </li>
+              );
+            })
+          }
         </ul>
       </div>
     );
@@ -151,22 +99,22 @@ const ItemType = React.createClass({
 
 const Movie = React.createClass({
   render () {
-  var self = this;
-	  return (
-	    <div className="wdy-view">
-	      <div className="bannar-all-box">
-	        <div id="slider_top" className="slider-box bannar" style={{height:'450px'}}>
-	            <MediaSlider {...MovieConfig['MediaSlider']}/>
-	        </div>
-	      </div>
+    var self = this;
+    return (
+      <div className="wdy-view">
+        <div className="bannar-all-box">
+          <div id="slider_top" className="slider-box bannar" style={{height:'450px'}}>
+            <MediaSlider {...MovieConfig['MediaSlider']}/>
+          </div>
+        </div>
         <div className='layout-center-box'>
           <Banner {...MovieConfig['Banner'][0]} />
         </div>
-        <ItemType listType={this.state.types} />
-	    </div>
-	  )
-	},
-	getInitialState: function() {
+        <ItemType type={this.state.types} />
+      </div>
+    )
+  },
+  getInitialState: function() {
     return {
       types: [MovieConfig['NewMovie'].baseUrl+MovieConfig['NewMovie'].dataUrl,
         MovieConfig['HotMovie'].baseUrl+MovieConfig['NewMovie'].dataUrl,
